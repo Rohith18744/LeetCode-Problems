54d895242d4c826d9b51a7dade872009bb67ec0f
Rohith18744 <115471244+Rohith18744@users.noreply.github.com>
2024-08-24 17:43:44 +0530

    Time: 41 ms (17.25%), Space: 16.6 MB (19.64%) - LeetHub

---
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.py
@@ -0,0 +1,26 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def swapPairs(self, head):
+        
+        dummy = ListNode(0)
+        dummy.next = head
+        current = dummy
+        
+        while current.next and current.next.next:
+            
+            first = current.next
+            second = current.next.next
+            
+            
+            first.next = second.next
+            second.next = first
+            current.next = second
+            
+            
+            current = first
+        
+        return dummy.next
